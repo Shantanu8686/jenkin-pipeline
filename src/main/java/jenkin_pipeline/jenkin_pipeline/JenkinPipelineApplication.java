96ffdab9b243be2f58ba82a4bb9ea47bfee4e1a6
shantanu8686 <shantanupardeshi3@gmail.com>
2025-01-07 01:40:26 +0530

    added code

--- src/main/java/jenkin_pipeline/jenkin_pipeline/JenkinPipelineApplication.java
+++ src/main/java/jenkin_pipeline/jenkin_pipeline/JenkinPipelineApplication.java
@@ -2,9 +2,17 @@ package jenkin_pipeline.jenkin_pipeline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class JenkinPipelineApplication {
+	@GetMapping("/message")
+	public String getMessage()
+	{
+		return "Jai Shree Ram";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinPipelineApplication.class, args);
